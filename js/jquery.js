49f00409297db343ed0a516000f5b9faeb576520
Juannito <juannito@gmail.com>
2014-09-19 15:18:14 -0300

    Galeria

--- js/jquery.js
+++ js/jquery.js
@@ -4,58 +4,50 @@ $(document).ready(function(){
     var $imgComp = $("<div id='comp'></div>");
     var $img = $("<img>");
     var $caption = $("<p></p>");
-    
+
     $imgComp.append($img);
     // Agrega la imagen al overlay
     $imgComp.append($caption);
     // Agrega la p al overlay
     $overlay.append($imgComp);
-    
+
     $("body").append($overlay);
-    
-//    console.log($("body"));
 
     // OCULTAR MODAL
     $overlay.hide();
-    
-    // CENTRAR
-    
-    var ventanaAncho = $(window).width();
-    var ventanaAlto = $(window).height();
-    
-    var modalAncho = $('#comp').width();
-    var modalAlto = $('#comp').height();
-    
-    console.log(modalAncho);
-    console.log(modalAlto);
-    
-    var modalTop = (ventanaAlto-modalAlto)/2;
-    var modalLeft = (ventanaAncho-modalAncho)/2;
-    
-    console.log(modalTop);
-    console.log(modalLeft);
 
-    $imgComp.css("top", modalTop);
-    $imgComp.css("left", modalLeft);
-    
-    console.log($imgComp);
-    
-//    $('#thumbs a').click(function(e){
+    // CLICK PARA VER
     $('a').click(function(e){
         e.preventDefault(); // Va solo para las A.
-        
+
         var $imageLocator = $(this).attr("href"); // attr sirve para obtener todos lso valores del elemento
         $img.attr("src", $imageLocator); // de esta manera se le pasa valores al atributo.
         var $textCaption = $(this).children("img").attr("alt"); // al hijo de la imagen.
         $caption.text($textCaption); // funcion para imprimir texto
-        
+
         // Mostrar modal
         $overlay.fadeIn();
-    }); 
-    
-    // OCULTAR
+
+        // CENTRAR
+        // ALTO VENTANA
+        var ventanaAncho = $(window).width();
+        var ventanaAlto = $(window).height();
+        // ALTO COMP
+        var modalAncho = $("#comp").width();
+        var modalAlto = $("#comp").height();
+
+        var modalTop = (ventanaAlto-modalAlto)/2;
+        var modalLeft = (ventanaAncho-modalAncho)/2;
+
+        $("#comp").css("top", modalTop);
+        $("#comp").css("left", modalLeft);
+
+        console.log($imgComp);
+    });
+
+    // OCULTAR IMAGEN
     $overlay.click(function(){ 
         $(this).fadeOut();
     }); 
-    
+
 });
